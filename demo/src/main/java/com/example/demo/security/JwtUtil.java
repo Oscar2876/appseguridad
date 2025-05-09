b57976bc154529eb5d11c7d725ef16b78d07a725
Oscar Zurita <oscar.zurita.est@tecazuay.edu.ec>
2025-05-08 20:33:03 -0500

    Guia Practia 1

---
+++ demo/src/main/java/com/example/demo/security/JwtUtil.java
@@ -0,0 +1,38 @@
+package com.example.demo.security;
+
+import org.springframework.stereotype.Component;
+import io.*;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+@Component
+public class JwtUtil {
+
+    private final String SECRET_KEY = "claveSecreta123";
+    private final long EXPIRATION_TIME = 86400000; // 1 día
+
+    public String generarToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public String obtenerUsernameDesdeToken(String token) {
+        return Jwts.parser()
+                .setSigningKey(SECRET_KEY)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public boolean validarToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
